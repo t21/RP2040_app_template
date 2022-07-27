f671b6b87796da71b1c06e0f8300faff28586851
Thomas Berg <thomas.berg2@gmail.com>
2022-07-27 16:41:21 +0000

    Clean up template application

--- apps/template/main.cpp
+++ apps/template/main.cpp
@@ -4,16 +4,17 @@
  * Copyright (c) 2021 Thomas Berg
  */
 
-#include "pico/printf.h"
-#include "pico/stdlib.h"
+#include "pico/time.h"
 
 #include "logger/Log.hpp"
 
-int main(void) {
-  // stdio_init_all();
-
+int main() {
   LOG_D("Template application started\r\n");
 
   while (true) {
+    LOG_D("Template application running\r\n");
+    sleep_ms(1000);
   }
+
+  return 0;
 }
